fffad851e923405a5a1f819b4167bc582be363ba
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-23 01:25:18 +0500

    DataTypes

---
+++ JavaScript/01 Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // now all code will treated according to the newer version of javascript
+
+// alert (3+5) #this will not work in nodejs, it will work in browser's software
+
+// number => 2^53
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// objects
+// symbol => unique (used in react etc.)
+
+let name = "Danyal";
+let age = 18;
+let isLoggedIn = false;
+let childName;
+
+console.log (typeof name);
+console.log (typeof age);
+console.log (typeof isLoggedIn);
+
+console.log (typeof childName);
+console.log (typeof undefined);
+console.log (typeof null);
