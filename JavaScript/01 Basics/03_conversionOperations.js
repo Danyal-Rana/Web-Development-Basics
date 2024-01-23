7a1b735810390b22c7bb2514bfa6a69cc2b7960e
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-23 23:36:19 +0500

    Operations

---
+++ JavaScript/01 Basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let frontEndScore = 33;
+let backEndScore = Number (frontEndScore);
+console.log (typeof (frontEndScore));
+console.log (typeof (backEndScore));
+console.log ();
+
+let frontEndScore1 = "33abc";
+let backEndScore1 = Number (frontEndScore1);
+console.log (typeof (frontEndScore1));
+console.log (typeof (backEndScore1));
+console.log (backEndScore1);
+console.log ();
+
+
+console.log ("1" + 2 + 3);
+console.log (1 + 2 + "3");
+
+console.log (-true);
+console.log (-false);
+
+let myCounter = 100;
+console.log (myCounter++);
+console.log (++myCounter);
