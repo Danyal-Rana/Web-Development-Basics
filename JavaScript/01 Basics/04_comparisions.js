ee96aadd6a0bbe65fd2932e7a9997a80217a1cfb
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-23 23:54:29 +0500

    Comparisions

--- JavaScript/01 Basics/04_comparisions.js
+++ JavaScript/01 Basics/04_comparisions.js
@@ -1,2 +1,10 @@
 console.log ("2" > 1);
 console.log ("02" > 1);
+
+// equality check (==) and comparisions operators (<, >, <=, >=) works differently
+
+console.log (null >= 0); // null is treated as 0
+console.log (null == 0);
+console.log (null < 0); // null is treated as 0
+
+
