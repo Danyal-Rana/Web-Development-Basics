93400b7bfd663fa558bff9626f96bacb6f000dde
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-24 23:53:08 +0500

    Numbers

---
+++ JavaScript/01 Basics/06_nums_&_math.js
@@ -0,0 +1,11 @@
+const score = 5;
+const balance = new Number (5); // now this is 100% number
+
+console.log (score);
+console.log (balance);
+
+console.log (balance.toString()); // now this is string
+console.log (balance.toFixed(2));
+
+const myNum = 99.958;
+console.log (myNum.toPrecision(3));
