5365c16446b841359755a3c3ae5b58334b67ff98
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-25 00:52:22 +0500

    Date

---
+++ JavaScript/01 Basics/07_dates.js
@@ -0,0 +1,13 @@
+// updates about Dates are amazing in JavaScript
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toDateString());
+console.log(myDate.toJSON());
+console.log(myDate.getUTCFullYear());
+
+let myDOB = new Date (2002, 11, 31);
+console.log(\n + myDOB);
