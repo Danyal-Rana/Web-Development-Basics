8039dfba3b9a92b1a3f7d48795df974dea56d96e
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-25 00:21:03 +0500

    Math

--- JavaScript/01 Basics/06_maths.js
+++ JavaScript/01 Basics/06_maths.js
@@ -6,3 +6,8 @@ console.log(Math.round(4.9));
 console.log(Math.ceil(4.1));
 console.log(Math.floor(4.9));
 
+let min = 1;
+let max = 6;
+
+const diceRandom = Math.floor ((Math.random()*(max-min+1)) + min);
+console.log(diceRandom);
