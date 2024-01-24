95867311dce23d2683732455a93be6803c9c2447
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-25 00:00:20 +0500

    Numbers

--- JavaScript/01 Basics/06_nums.js
+++ JavaScript/01 Basics/06_nums.js
@@ -9,3 +9,10 @@ console.log (balance.toFixed(2));
 
 const myNum = 99.958;
 console.log (myNum.toPrecision(3));
+
+const anotherNum = 100000000;
+console.log (anotherNum.toLocaleString());
+console.log (anotherNum.toLocaleString("en-PK"));
+
+console.log (Number.MAX_VALUE);
+console.log (Number.MAX_SAFE_INTEGER);
