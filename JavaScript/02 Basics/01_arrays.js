15e3a081463d3bc1753ddd320268f93a320d8f15
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-25 23:58:28 +0500

    push/pop and shift/unshift

--- JavaScript/02 Basics/01_arrays.js
+++ JavaScript/02 Basics/01_arrays.js
@@ -5,3 +5,17 @@ console.log (myArr[0]);
 
 const myArr2 = new Array (1, 2, 3, 4, 5);
 console.log (myArr2[5]); // undefined
+
+// Methods
+myArr.push (8);
+console.log(myArr);
+myArr.pop ();
+console.log (myArr);
+console.log();
+
+myArr.unshift (100); // not recemmended, as it shifts all the values
+console.log (myArr);
+myArr.shift();
+console.log (myArr);
+console.log();
+
