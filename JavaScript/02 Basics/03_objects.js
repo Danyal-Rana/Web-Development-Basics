f604989cd581d131a41b9559f223977815a5178a
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-27 00:32:38 +0500

    Objects

---
+++ JavaScript/02 Basics/03_objects.js
@@ -0,0 +1,11 @@
+// 2 types of object
+
+// #1 
+
+// #2 object literals, discussed here
+
+const JsUser = { //each key is treated as string in javascript
+    name: "Danyal",
+    age: 18,
+    "city": "Rohillan Wali"
+}
