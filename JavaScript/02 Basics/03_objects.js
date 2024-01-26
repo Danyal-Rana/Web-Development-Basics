c6c86c729f9f5b815346882631464b421c66cb8a
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-01-27 00:39:25 +0500

    Objects

--- JavaScript/02 Basics/03_objects.js
+++ JavaScript/02 Basics/03_objects.js
@@ -4,12 +4,18 @@
 
 // #2 object literals, discussed here
 
+const mySym = Symbol ("Key1");
+
 const JsUser = { //each key is treated as string in javascript
     name: "Danyal",
     age: 18,
-    "city": "Rohillan Wali"
+    "city": "Rohillan Wali",
+
+    mySym: "myKey",
 }
 
 console.log(JsUser);
 console.log(JsUser.age); // not the official way
 console.log(JsUser["name"]); // recommended
+
+console.log (JsUser["mySym"]);
