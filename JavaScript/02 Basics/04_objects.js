2037e6022fa9ee74cb07d47c403fa6693d7990a0
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-14 17:42:01 +0500

    Learnign

--- JavaScript/02 Basics/04_objects.js
+++ JavaScript/02 Basics/04_objects.js
@@ -34,7 +34,7 @@ const obj2 = {3: "c", 4: "d"};
 const obj3 = {obj1, obj2}; // not recommended
 console.log (obj3);
 
-const obj4 = Object.assign({}, obj1, obj2); // {} acts as target object which is returned, and obj1 & obj2 are treated as source object
+const obj4 = Object.assign({}, obj1, obj2); // {} acts as target object which is returned, and obj1 & obj2 are treated as source objects
 console.log(obj4);
 
 const obj5 = {...obj1, ...obj2}; // mostly used
@@ -56,6 +56,6 @@ console.log(users[0].name);
 
 console.log (Object.keys(tinderUser)); // return array which can be utilized further
 console.log (Object.values(tinderUser)); // also return array of values
-console.log (Object.entries(tinderUser)); // return arrays of array(key value pairs)
+console.log (Object.entries(tinderUser)); // return key value pairs in the form of array
 
 console.log (Object.hasOwnProperty("names"));
