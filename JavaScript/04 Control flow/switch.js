8d3c34b52a8198ba7820d0494882715fa8adef8d
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-15 18:55:15 +0500

    Learning

---
+++ JavaScript/04 Control flow/switch.js
@@ -0,0 +1,45 @@
+const month = 64;
+
+// if we don't use switch, all the below cases will be executed EXCEPT default
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;        
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+
+    default:
+        console.log("Not a valid Month");
+}
