479bea7b585cd4c71c9e3f534b399ed8adcb3e9f
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-15 23:16:41 +0500

    Learning

---
+++ JavaScript/05 Iterations/two.js
@@ -0,0 +1,11 @@
+// while and do-while loop
+
+let i = 5;
+while (i == 5) {
+    console.log ("This is while loop in which value of i is 5");
+    i++;
+}
+
+do {
+    console.log ("This is do-while loop which executes atleast once.")
+} while (2 === 3);
