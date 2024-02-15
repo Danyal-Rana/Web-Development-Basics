2144e74dea2d9ad574b2e35f071ab276a762439e
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-15 23:23:38 +0500

    Learning

--- JavaScript/05 Iterations/three.js
+++ JavaScript/05 Iterations/three.js
@@ -1,2 +1,13 @@
 // these loops are arry specific loops
 
+
+// for of
+let arr = [1, 2, 3, 4, 5];
+for (const i of arr) {
+    console.log (i);
+}
+
+const myName = "Danyal Rana";
+for (let i of myName) { // i can also use let, but const is recommended
+    console.log (i);
+}
