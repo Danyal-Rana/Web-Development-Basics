916d327be1c425a1e027ecddc02cb6f378481259
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-16 00:16:35 +0500

    Learning

--- JavaScript/05 Iterations/three.js
+++ JavaScript/05 Iterations/three.js
@@ -11,3 +11,23 @@ const myName = "Danyal Rana";
 for (let i of myName) { // i can also use let, but const is recommended
     console.log (i);
 }
+
+const map = new Map ();
+map.set ("Pk", "Pakistan");
+map.set ("Pk", "Pakistan"); // this will not be counted
+map.set ("USA", "United States of America");
+map.set ("UK", "danyal"); // this will be discarded
+map.set ("UK", "United Kingdom");
+map.set ("United Kingdom", "UK");
+console.log (map);
+
+for (const i of map) {
+    console.log (i);
+}
+
+for (const [i, j] of map) {
+    console.log (i);
+    console.log (i ,":=>", j);
+}
+
+//in this way for-of loop does not work for objects
