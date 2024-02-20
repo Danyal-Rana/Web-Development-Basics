4b733a1e611c0860103de94d437ec112ce8ab5ae
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-20 19:46:10 +0500

    bg color changer

--- JavaScript/07 Projects/01-ColorChanger/firstProject.js
+++ JavaScript/07 Projects/01-ColorChanger/firstProject.js
@@ -1,1 +1,20 @@
 const myButtons = document.querySelectorAll(".button");
+const documentBody = document.querySelector("body");
+
+myButtons.forEach (function (iButton) {
+    iButton.addEventListener('click', function (iEvent) {
+        
+        if (iEvent.target.id === 'grey') {
+            documentBody.style.backgroundColor = iEvent.target.id;
+        }
+        else if (iEvent.target.id === 'white') {
+            documentBody.style.backgroundColor = iEvent.target.id;
+        }
+        else if (iEvent.target.id === 'blue') {
+            documentBody.style.backgroundColor = iEvent.target.id;
+        }
+        else if (iEvent.target.id === 'yellow') {
+            documentBody.style.backgroundColor = iEvent.target.id;
+        }
+    } )
+})
