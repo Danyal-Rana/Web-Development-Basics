f02a713e780255570c1524fefe0e6418ddf9b7bb
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-23 15:28:52 +0500

    guess

--- JavaScript/07 Projects/04-GuessTheNumber/number.js
+++ JavaScript/07 Projects/04-GuessTheNumber/number.js
@@ -6,3 +6,21 @@ const guessSlot = document.querySelector(".guessses");
 const lastResult = document.querySelector(".lastResult");
 const lowOrHigh = document.querySelector(".lowOrHigh");
 const startOver = document.querySelector(".resultParas");
+
+const p = document.createElement('p');
+
+let preGuess = [];
+let numGuess = 1;
+let playGame = true;
+
+function isValid (guess) {
+
+}
+
+function checkGuess (guess) {
+
+}
+
+function displayMessage (guess) {
+
+}
