f0b132abd6c79f56021e0a4bfef111169f979a8b
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-02-23 15:17:38 +0500

    selectors

---
+++ JavaScript/07 Projects/04-GuessTheNumber/number.js
@@ -0,0 +1,6 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submitButton = document.querySelector('#subt');
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guessses");
+const lastResult = document.querySelector(".lastResult");
