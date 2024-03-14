779c34d3394e4302f17fbc8e6992530f568a2e73
Muhammad Danyal Rana <ranadanyalarshad@gmail.com>
2024-03-14 16:01:58 +0500

    inheritence

---
+++ JavaScript/10 OOP/inheritence.js
@@ -0,0 +1,20 @@
+class User {
+    constructor (username) {
+        this.username = username;
+    }
+}
+
+class Student extends User {
+    constructor (username, age, mail) {
+        super (username);
+        this.age = age;
+        this.mail = mail;
+    }
+
+    toString () {
+        console.log (`Name is ${this.username}, age is ${this.age} and email is ${this.email}`);
+    }
+}
+
+const s = new Student ("Danyal", 12, "admin@mdrana.com");
+console.log (s);
