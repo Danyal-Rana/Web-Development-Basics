ee796ac82ca49858a0d5a0c07cd7d5544cccf735
Danyal Rana <ranadanyalarshad@gmail.com>
2024-12-09 14:53:45 +0500

    Add Form, FilterButton, and Todo components for task management

---
+++ CUI-Labs/Lab-10/src/components/FilterButton.jsx
@@ -0,0 +1,16 @@
+function FilterButton() {
+    const text = ["All", "Active", "Completed"];
+    return (
+        <div>
+            {text.map((buttonText, idx) => (
+                <button type="button" className="btn toggle-btn" aria-pressed="true">
+                    <span className="visually-hidden">Show </span>
+                    <span>Show {buttonText}</span>
+                    <span className="visually-hidden"> tasks</span>
+                </button>
+            ))}
+        </div>
+    );
+}
+
+export default FilterButton;
